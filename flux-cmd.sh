172e83e53443ec01c5fede9ae323186377bf88fc
rohinp <rohin.patel@outlook.com>
2024-09-08 18:47:14 +0200

    Add flux command.

---
+++ flux-cmd.sh
@@ -0,0 +1,16 @@
+flux bootstrap github \
+  --owner=your-username \
+  --repository=home-lab-repo \
+  --branch=main \
+  --path=clusters/microk8s/flux-system \
+  --token-auth
+
+
+
+
+flux bootstrap github \
+  --owner=rohinp \
+  --repository=home-lab \
+  --branch=main \
+  --path=clusters/microk8s/flux-system \
+  --token-auth 
