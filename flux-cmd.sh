abd55f5d71428ac9fc432b7250d7d4fc2948d02e
rohinp <rohin.patel@outlook.com>
2024-09-10 10:10:14 +0200

    Update interval for kometa.

--- flux-cmd.sh
+++ flux-cmd.sh
@@ -1,16 +1,6 @@
 flux bootstrap github \
-  --owner=your-username \
-  --repository=home-lab-repo \
-  --branch=main \
-  --path=clusters/microk8s/flux-system \
-  --token-auth
-
-
-
-
-flux bootstrap github \
   --owner=rohinp \
   --repository=home-lab \
   --branch=main \
   --path=clusters/microk8s/flux-system \
-  --token-auth 
+  --token-auth
